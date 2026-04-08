0847d3d3c289150ea419edbfb1dee10e88d37035
sriom-singh <sriomsingh543@gmail.com>
2026-04-08 23:28:49 +0530

    minor change to fix bug

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='mt-16 min-h-screen'>About</div>
+    <div className='mt-16 min-h-screen'>About US</div>
   )
 }
 
--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -2,8 +2,8 @@
 export default function NotFound() {
   return (
     <div className="h-screen flex items-center justify-center flex-col">
-      <h1 className="text-4xl font-bold">404</h1>
-      <p className="text-muted-foreground">Page not found</p>
+      <h1 className="text-6xl font-bold font-sans-serif mb-2">404</h1>
+      <p className="text-muted-foreground font-sans-serif text-xl">Page not found</p>
     </div>
   );
 }
